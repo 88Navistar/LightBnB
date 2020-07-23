94091239485f35aea4352ec0e666cdeb35ad08b8
Rob Wallator <rob@rawimpressions.ca>
2020-07-23 20:13:46 +0000

    get all reservations functionallity

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -71,7 +71,21 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const text = `SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2`;
+  const value = [guest_id, limit];
+
+  return pool
+    .query(text, values)
+    .then(res => res.rows)
+    .catch(error => console.error('Error', error.stack));
 }
 exports.getAllReservations = getAllReservations;
 
